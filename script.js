78b2d76bdaef61527112b8917c0a0e194bd937df
Katherine Tsai <katherinetsai@DESKTOP-10SDEV1.localdomain>
2022-10-21 18:00:39 -0400

    add event listener in js

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by Javascript";
 
 document.querySelector("body").appendChild(h2);
+
+Image.addEventLister('mouseover', "Nice to meet you!");
